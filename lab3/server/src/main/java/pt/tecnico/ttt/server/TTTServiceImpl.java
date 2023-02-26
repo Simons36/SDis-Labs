0877c925a3b6f5b342b66cf25902a9317b42694e
Simão Silva <58431058+Simons36@users.noreply.github.com>
2023-02-26 18:14:08 +0000

    Add files via upload

---
+++ lab3/server/src/main/java/pt/tecnico/ttt/server/TTTServiceImpl.java
@@ -0,0 +1,46 @@
+package pt.tecnico.ttt.server;
+
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.ttt.*;
+
+public class TTTServiceImpl extends TTTGrpc.TTTImplBase {
+
+	/** Game implementation. */
+	private TTTGame ttt = new TTTGame();
+
+	@Override
+	public void currentBoard(CurrentBoardRequest request, StreamObserver<CurrentBoardResponse> responseObserver) {
+		// StreamObserver is used to represent the gRPC stream between the server and
+		// client in order to send the appropriate responses (or errors, if any occur).
+
+		CurrentBoardResponse response = CurrentBoardResponse.newBuilder().setBoard(ttt.toString()).build();
+
+		// Send a single response through the stream.
+		responseObserver.onNext(response);
+		// Notify the client that the operation has been completed.
+		responseObserver.onCompleted();
+	}
+
+
+	@Override
+	public void playResult(PlayResultRequest request, StreamObserver<PlayResultResponse> responseObserver){
+		PlayResult playResultResponse = ttt.play(request.getRow(), request.getColumn(), request.getPlayer());
+
+		PlayResultResponse response = PlayResultResponse.newBuilder().setPlResult(playResultResponse).build();
+
+		responseObserver.onNext(response);
+
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void checkWinner(CheckWinnerRequest request, StreamObserver<CheckWinnerResponse> responseObserver){
+		int winnerResponse = ttt.checkWinner();
+
+		CheckWinnerResponse response = CheckWinnerResponse.newBuilder().setWinner(winnerResponse).build();
+
+		responseObserver.onNext(response);
+		responseObserver.onCompleted();
+	}
+
+}
